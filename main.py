d838bd9b8840ba7054ebad1b768928361a9e5010
jordanpang <jp1222@ic.ac.uk>
2023-08-30 18:21:00 +0100

    Comments

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
-symbol_count = { #This is a dictionary of how many symbols in each reel / column
+symbol_count = { #This is a dictionary of how many possible symbols in each reel / column
     "A": 2,
     "B": 4,
     "C": 6,
@@ -23,13 +23,14 @@ symbol_value = { #This is a dictionary of each symbol with the respective multip
 
 #In all these functions, we work on the slot machine with the rows and columns switched
 
-def deposit(): #This is the amount that the user will bet with
-    while True: #Continue this loop until we break out, i.e. until we get a valid deposit input
+def deposit():
+    """Return the amount that the user will bet with."""
+    while True:
         amount = input("What would you like to deposit? $")
-        if amount.isdigit(): #This checks whether the input is a positive full number
-            amount = int(amount) #By default, the user input is a string
+        if amount.isdigit():
+            amount = int(amount) #Since by default, the user input was a string
             if amount > 0:
-                break #Breaks out of loop
+                break
             else:
                 print("Amount must be greater than 0.")
         else:
@@ -37,12 +38,13 @@ def deposit(): #This is the amount that the user will bet with
     return amount
 
 def get_number_of_lines():
-    while True: #Continue this loop until we break out, i.e. until we get a valid deposit input
-        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + "): ") #Concatenation for strings
-        if lines.isdigit(): #This checks whether the input is a positive full number
-            lines = int(lines) #By default, the user input is a string
+    """Returns the number of lines that will be betted on."""
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + "): ")
+        if lines.isdigit():
+            lines = int(lines)
             if 1 <= lines <= MAX_LINES:
-                break #Breaks out of loop
+                break
             else:
                 print("Enter a valid number of lines.")
         else:
@@ -50,6 +52,7 @@ def get_number_of_lines():
     return lines
 
 def get_bet():
+    """Return the user's bet."""
     while True:
         bet = input("Enter the bet that you would like to place on each line (" + str(MIN_BET)+ "-" + str(MAX_BET) + ")")
         if bet.isdigit():
@@ -62,41 +65,39 @@ def get_bet():
             print("Enter a positive number")
     return bet
 
-def get_slot_machine_spin(rows, cols, symbols): #We are passing these parameters through to later on
-    #We would like to put all the symbols into a dictionary as individual elements
+def get_slot_machine_spin(rows, cols, symbols):
+    """Return all the columns after spin, using symbols as a dictionary."""
     all_symbols = [] #Creating a list of all symbols for each reel / column
-    for symbol, symbol_count in symbols.items(): # Looping through the dictionary, key is symbol and value is symbol_count. symbols is a new dictionary
-        #.items gives you the keys and values associated to a dictionary
-        for _ in range(symbol_count): # _ so you don't care about what you are iterating
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
             all_symbols.append(symbol)
 
-    columns = [] # All the columns, i.e. a list of spins for each column
-    for _ in range(cols): #_ is just a junk variable (unused)
-        column = [] #The spin in each column
-        current_symbols = all_symbols[:] #A copy of all_symbols. Slice
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:] #Creates a copy of all_symbols by slice
         for _ in range(rows):
-            value = random.choice(current_symbols) #picks a random value from the list
+            value = random.choice(current_symbols)
             current_symbols.remove(value) 
             column.append(value)
-        #Now you have a complete spin for a column
         columns.append(column)
-    #Now you have spins for each column
     return columns
 
 def print_slot_machine(columns):
-    #Want to transpose
-    for row in range(len(columns[0])): #Assuming we have at least one row in the slot machine
-        for i, column in enumerate(columns): #Looking at each column in columns
+    """Transpose columns for user visualisation."""
+    for row in range(len(columns[0])): #Assume we have at least one row in the slot machine
+        for i, column in enumerate(columns):
             if i != len(columns) - 1:   
                 print(column[row], end = " | ") #pipe operator to separate each item
             else:
                 print(column[row], end = "")
         
-        print() #Makes it go onto new line after we finish the row
+        print() #Separate each row
 
-def spin(balance): #The balance comes from the main function
+def spin(balance):
+    """Returns the profit/loss from spinning."""
     lines = get_number_of_lines()
-    while True: #Checking if the bet is within the balance
+    while True:
         bet = get_bet()
         total_bet = bet * lines
         if total_bet > balance:
@@ -104,15 +105,15 @@ def spin(balance): #The balance comes from the main function
         else:
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
-    #Okay so we have decided what they are betting
-    slots = get_slot_machine_spin(ROWS, COLS, symbol_count) #Spin the slot machine
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
     winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
     print(f"You won ${winnings}.")
-    print(f"You won on lines:", *winning_lines) #splat / unpack operator
+    print(f"You won on lines:", *winning_lines) #Splat / unpack operator
     return winnings - total_bet
 
-def check_winnings(columns, lines, bet, values): #Want to check if the first symbol in each row is the same in each column with same row
+def check_winnings(columns, lines, bet, values):
+    """Return winnings and respective lines in the bet."""
     winnings = 0
     winning_lines = []
     for line in range(lines):
@@ -127,6 +128,7 @@ def check_winnings(columns, lines, bet, values): #Want to check if the first sym
     return winnings, winning_lines
 
 def main():
+    """Put the game together and plays it."""
     balance = deposit()
     while True:
         print(f"Current balance is: ${balance}")
